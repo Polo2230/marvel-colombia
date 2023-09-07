946af8bcbb62780de494b4bb55e5a5d79b2d75bd
Polo2230 <johans.polon@autonoma.edu.co>
2023-09-07 14:46:05 -0500

    initial structure

---
+++ marvel.js
@@ -0,0 +1,53 @@
+const consultaMarvel = () => {
+  const apiUrl = "https://gateway.marvel.com/v1/public/series";
+  const apiKey = "65f08d74236aae47779473fd02b1f0d3";
+  const ts = "1";
+  const hash = "3c6fc4ce58ff953d1a65a8ee2de31037";
+
+  const url = `${apiUrl}?ts=${ts}&apikey=${apiKey}&hash=${hash}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const moviesList = data.data.results;
+      console.log(moviesList)
+      const moviesContainer = document.querySelector(".movies-list")
+      moviesContainer.innerHTML = ""
+      moviesList.forEach((moviesList) => {
+        const moviesCard = document.createElement("div")
+        const movieName = document.createElement("p")
+        const moviesUrl= moviesList.urls
+        movieName.textContent = moviesList.title
+        moviesUrl.forEach((moviesUrl)=>{
+          const moviesUrls= document.createElement("p")
+          moviesUrls.textContent= moviesUrl.url
+          moviesCard.appendChild(moviesUrls)
+          moviesCard.appendChild(movieName)
+          moviesContainer.appendChild(moviesCard)
+          moviesList.forEach()
+        })
+        
+      });
+
+    })
+
+
+    .catch(error => {
+      console.error("Error:", error);
+    });
+}
+
+
+/*   .then(response => response.json())
+.then(data => {
+  data.data.results.forEach(item => {
+    console.log("Title:", item.title);
+
+    console.log("URLs:");
+    item.urls.forEach(urlInfo => {
+      console.log(urlInfo.url);
+    });
+
+    console.log("--------");
+  });
+}) */
