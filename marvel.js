39ece2c61b936446a34bbbd796c16d80cd5c50f9
luisemecasa <luism.canons@autonoma.edu.co>
2023-09-07 21:29:57 -0500

    table creation and styles modification

--- marvel.js
+++ marvel.js
@@ -1,27 +1,32 @@
 const consultaMarvel = () => {
-  const apiUrl = "https://gateway.marvel.com/v1/public/series";
-  const apiKey = "65f08d74236aae47779473fd02b1f0d3";
-  const ts = "1";
-  const hash = "3c6fc4ce58ff953d1a65a8ee2de31037";
-
-  const url = `${apiUrl}?ts=${ts}&apikey=${apiKey}&hash=${hash}`;
+  const url = "http://gateway.marvel.com/v1/public/comics?ts=1&apikey=9aa560a9a861b89b42bb33af60c12977&hash=429255a25aa4af880c6f79f4bd2ddba2";
 
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      const moviesList = data.data.results;
-      console.log(moviesList)
-      const moviesContainer = document.querySelector(".movies-list")
-      moviesContainer.innerHTML = ""
+      const moviesList = data.data.results;      
+      const moviesTable = document.getElementById('comicTable')
       moviesList.forEach((moviesList) => {
-        const moviesCard = document.createElement("div")
-        const movieStories= moviesList.stories.items    
+        const row = document.createElement("tr")
+        const title = document.createElement("td") 
+        title.textContent = moviesList.title
+        const movieStories= moviesList.stories.items
+        const movieCreators= moviesList.creators.items  
         movieStories.forEach((movieStories)=>{
-          const Storiesname= document.createElement("p")
-          Storiesname.textContent= movieStories.name
-          moviesCard.appendChild(Storiesname)
+          const Storiesname= document.createElement("td")
+          const Stories= movieStories.map(story=>story.name).join("-")
+          Storiesname.textContent = Stories
+          row.appendChild(Storiesname)
           console.log(Storiesname)  
         }) 
+        movieCreators.forEach((movieCreators)=>{
+          const creators = document.createElement("td")
+          const creatorsName= movieCreators.map(creator => creator.name).join(' - ')
+          creators.textContent = creatorsName
+          row.appendChild(creators)
+          console.log(creatorsName)
+        })
+        moviesTable.append(row)
         
       });
 
@@ -31,18 +36,3 @@ const consultaMarvel = () => {
       console.error("Error:", error);
     });
 }
-
-
-/*   .then(response => response.json())
-.then(data => {
-  data.data.results.forEach(item => {
-    console.log("Title:", item.title);
-
-    console.log("URLs:");
-    item.urls.forEach(urlInfo => {
-      console.log(urlInfo.url);
-    });
-
-    console.log("--------");
-  });
-}) */
